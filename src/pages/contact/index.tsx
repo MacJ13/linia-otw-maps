30f5dd1195c77ae78d649f240ea42cf535e01b64
MacJ13 <jacdim02@gmail.com>
2024-03-11 23:58:51 +0100

    Add content element to Contact component

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,7 +2,27 @@
 import style from "./Contact.module.scss";
 
 const Contact = () => {
-  return <div className={style.contact}></div>;
+  return (
+    <div className={style.contact}>
+      <div className={style.content}>
+        <div className={style.header}>
+          <h1 className={style.h1}>Kontakt</h1>
+        </div>
+
+        <div className={style.wrapper}>
+          <p className={style.p}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem ipsum has been the industry text ever since when
+            unknown printer sdfsd imply dummy text of the printing and
+            typesetting industry
+          </p>
+          <div className={style.mail}>
+            <strong>example@gmail.com</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
