5c0bc6563125b0198716fc7b7592e276f8ae06d1
MacJ13 <jacdim02@gmail.com>
2024-04-03 17:10:11 +0200

    Add vite-plugin=svgr package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // base: "/maps-linia-otw/",
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
+});
