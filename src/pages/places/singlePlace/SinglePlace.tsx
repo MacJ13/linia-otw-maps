34a7b5c55a9cecd9afc577e9f17314a0a3930c3a
MacJ13 <jacdim02@gmail.com>
2024-04-09 22:48:13 +0200

    Update SinglePlace style component

--- src/pages/places/singlePlace/SinglePlace.tsx
+++ src/pages/places/singlePlace/SinglePlace.tsx
@@ -1,4 +1,7 @@
 import style from "./SinglePlace.module.scss";
+import Ornament from "../../../assets/svg/swidermajer.svg?react";
+import placeImg from "../../../assets/images/place.jpg";
+// import OrnamentImg from "../../../assets/svg/swidermayer.svg?react";
 
 type SinglePlaceProps = {
   reverse: boolean;
@@ -7,22 +10,22 @@ type SinglePlaceProps = {
 const SinglePlace = ({ reverse }: SinglePlaceProps) => {
   return (
     <div className={style.wrapper}>
-      <div className={style.place}>
-        <div className={`${style.card} ${reverse ? style.reverse : ""}`}>
-          <div className={style.photo}>photo</div>
-          <div className={style.info}>
-            <div className={style.heading}>
-              <h3 className={style.h3}>Fortyfikacje</h3>
-            </div>
-
-            <p className={style.p}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem ipsum has been the industry text ever since when
-              unknown printer sdfsd sdg sdg sdg sdg sdng kjsdg jksdg kj sdjsdjkg
-              hksdjg hkjg lsdgj lksdgj lksdgj lksdgj lkj
-            </p>
-            <a className={style.link}>Zobacz na mapie</a>
-          </div>
+      <div className={`${style.card} ${reverse ? style.reverse : ""}`}>
+        <div className={style.ornament}>
+          <Ornament />
+        </div>
+        <div className={`${style.content}`}>
+          <h3 className={style.h3}>Fortyfikacje</h3>
+          <p className={`paragraph ${style.p}`}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem ipsum has been the industry text ever since when
+            unknown printer sdfsd sdg sdg sdg sdg sdng kjsdg jksdg kj sdjsdjkg
+            hksdjg hkjg lsdgj lksdgj lksdgj lksdgj lkj
+          </p>
+          <a className={`link ${style.link}`}>Zobacz na mapie</a>
+        </div>
+        <div className={style.photo}>
+          <img src={placeImg} alt="image test" />
         </div>
       </div>
     </div>
