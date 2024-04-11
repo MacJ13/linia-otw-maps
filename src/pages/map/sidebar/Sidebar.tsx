f222bdd94298bc9cfd86621de86bd1d7b6157924
MacJ13 <jacdim02@gmail.com>
2024-04-11 22:19:38 +0200

    Toggle sidebar feature elements

--- src/pages/map/sidebar/Sidebar.tsx
+++ src/pages/map/sidebar/Sidebar.tsx
@@ -1,19 +1,55 @@
+import { useState } from "react";
 import style from "./Sidebar.module.scss";
 
 const Sidebar = () => {
+  const [activeFeature, setActiveFeature] = useState<number>(0);
+
+  //   const onButtonClick = (activeNumber: number) => {
+  //     setActiveFeature(activeNumber);
+  //   };
+
   return (
     <div className={style.sidebar}>
       <div className={style.features}>
         <div
           id="places"
-          className={`${style.feature} ${style.active__feature}`}
+          className={
+            activeFeature === 0
+              ? `${style.feature} ${style.active__feature}`
+              : style.feature
+          }
         >
-          <button className={`link ${style.link}`}>Miejsca</button>
-          <div className={style.content}></div>
+          <button
+            onClick={() => {
+              setActiveFeature(0);
+            }}
+            className={`link ${style.link}`}
+          >
+            Miejsca
+          </button>
+          {activeFeature === 0 && <div className={style.content}></div>}
         </div>
-        <div className={style.feature}>
-          <button className={`link ${style.link}`}>Historyczne mapy</button>
+        <div
+          className={
+            activeFeature === 1
+              ? `${style.feature} ${style.active__feature}`
+              : style.feature
+          }
+        >
+          <button
+            onClick={() => {
+              setActiveFeature(1);
+            }}
+            className={`link ${style.link}`}
+          >
+            Historyczne mapy
+          </button>
           {/* <div className={style.content}></div> */}
+          {activeFeature === 1 && (
+            <div className={style.content}>
+              sdgsdg sdghsdhsdh sdghsdhsdhsd hasdgsdg
+            </div>
+          )}
         </div>
 
         <button className={`link ${style.link}`}>Warstwa mapy</button>
