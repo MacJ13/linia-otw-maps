68e7368c8d3cf0e888308003eb9216ece85e2e93
MacJ13 <jacdim02@gmail.com>
2024-04-17 19:56:54 +0200

    Move MapButton component to components folder

--- src/pages/map/components/button/MapButton.tsx
+++ src/pages/map/components/button/MapButton.tsx
@@ -1,9 +1,5 @@
+import { MapButtonProps } from "../../../types/map";
 import style from "./MapButton.module.scss";
-
-type MapButtonProps = {
-  name: string;
-  handleClick: () => void;
-};
 
 const MapButton = ({ name, handleClick }: MapButtonProps) => {
   return (
