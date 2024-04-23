d03453301372d19818a77a9c71e4fca9919dc83d
MacJ13 <jacdim02@gmail.com>
2024-04-23 22:27:06 +0200

    Update historic Layer reducer with Entity Adapter

--- src/pages/map/state/historicLayerSlice.ts
+++ src/pages/map/state/historicLayerSlice.ts
@@ -1,22 +1,46 @@
-import { PayloadAction, createSlice, nanoid } from "@reduxjs/toolkit";
+import {
+  PayloadAction,
+  createEntityAdapter,
+  createSlice,
+  nanoid,
+} from "@reduxjs/toolkit";
 import { RootState } from "./store";
 import { HistoricLayer } from "../../../types/map";
 
 // const state = {};
 
+const historicLayerAdapter = createEntityAdapter({
+  selectId: (historicLayer: HistoricLayer) => historicLayer.layerId,
+});
+
 const historicLayerSlice = createSlice({
   name: "historicLayer",
-  initialState: {
-    activeLayers: [] as HistoricLayer[],
-  },
+  initialState: historicLayerAdapter.getInitialState(),
+  // initialState: {
+  //   activeLayers: [] as HistoricLayer[],
+  // },
   reducers: {
     addLayer: {
       reducer: (state, action: PayloadAction<HistoricLayer>) => {
-        state.activeLayers.push(action.payload);
+        const { layerId } = action.payload;
+
+        const existId = state.entities[layerId];
+
+        if (existId) return;
+        console.log(existId);
+
+        historicLayerAdapter.addOne(state, action.payload);
       },
-      prepare(name: string, layers: string, url: string, format: string) {
+      prepare(
+        id: string,
+        name: string,
+        layers: string,
+        url: string,
+        format: string
+      ) {
         return {
           payload: {
+            layerId: id,
             id: nanoid(),
             name,
             layers,
@@ -29,17 +53,28 @@ const historicLayerSlice = createSlice({
       },
     },
     removeLayer(state, action: PayloadAction<string>) {
-      const filteredLayers = state.activeLayers.filter(
-        (layer) => action.payload !== layer.id
-      );
+      historicLayerAdapter.removeOne(state, action.payload);
+      // const filteredLayers = state.activeLayers.filter(
+      //   (layer) => action.payload !== layer.id
+      // );
 
-      state.activeLayers = filteredLayers;
+      // state.activeLayers = filteredLayers;
     },
   },
 });
 
 export const { addLayer, removeLayer } = historicLayerSlice.actions;
 
-export const selectAllActiveLayers = (state: RootState) =>
-  state.historicLayer.activeLayers;
+// export const selectAllActiveLayers = (state: RootState) =>
+//   state.historicLayer.activeLayers;
+
+export const {
+  selectAll: selectAllActiveLayers,
+  selectById: selectActiveLayerById,
+  selectIds: selectActiveLayerIds,
+  selectTotal: selectTotalActiveLayers,
+} = historicLayerAdapter.getSelectors(
+  (state: RootState) => state.historicLayer
+);
+
 export default historicLayerSlice.reducer;
