f16fb82923ea422547c0e3cd3b9e785cfe01706f
MacJ13 <jacdim02@gmail.com>
2024-05-31 19:46:49 +0200

    Deploy github pages with react router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,34 +6,37 @@ import Contact from "../pages/contact";
 import Places from "../pages/places";
 import Project from "../pages/project";
 
-const routerConfig = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        element: <Home />,
-        index: true,
-      },
-      {
-        path: "map",
-        element: <Map />,
-      },
-      {
-        path: "project",
-        element: <Project />,
-      },
-      {
-        path: "places",
-        element: <Places />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+const routerConfig = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: "map",
+          element: <Map />,
+        },
+        {
+          path: "project",
+          element: <Project />,
+        },
+        {
+          path: "places",
+          element: <Places />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/linia-otw-maps/" }
+);
 
 const Router = () => {
   return <RouterProvider router={routerConfig} />;
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,30 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // base: "/maps-linia-otw/",
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg?react",
-    }),
-  ],
+// export default defineConfig({
+//   // base: "/maps-linia-otw/",
+//   plugins: [
+//     react(),
+//     svgr({
+//       include: "**/*.svg?react",
+//     }),
+//   ],
+// });
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg?react",
+      }),
+    ],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/linia-otw-maps";
+  }
+
+  return config;
 });
